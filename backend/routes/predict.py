5a286992f15722532da9eb199a749f42f7d3ea3b
Samyakborkar12 <samyakborkar35@gmail.com>
2026-07-07 13:33:49 +0530

    Complete ML prediction pipeline with modular backend architecture

--- backend/routes/predict.py
+++ backend/routes/predict.py
@@ -1,14 +1,24 @@
 from flask import Blueprint, request, jsonify
-
 from backend.services.prediction_service import predict_radiation
 
 predict = Blueprint("predict", __name__)
 
-
 @predict.route("/api/predict", methods=["POST"])
 def predict_api():
 
-    data = request.get_json()
+    print("Content-Type:", request.content_type)
+    print("Headers:", request.headers)
+    print("Raw Data:", request.data)
+
+    data = request.get_json(silent=True)
+
+    print("JSON:", data)
+
+    if data is None:
+        return jsonify({
+            "status": "error",
+            "message": "No JSON received"
+        }), 400
 
     result = predict_radiation(data)
 
